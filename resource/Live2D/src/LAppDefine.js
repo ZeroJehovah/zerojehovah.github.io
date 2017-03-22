38740e555747fb68cf5a37bde2ec54be6055900e
ZeroJehovah <zj294753618@gmail.com>
2017-03-22 16:15:24 +0800

    Live2D

---
+++ resource/Live2D/src/LAppDefine.js
@@ -0,0 +1,34 @@
+var LAppDefine = {
+
+    DEBUG_LOG : true,
+    DEBUG_MOUSE_LOG : false,
+
+    VIEW_MAX_SCALE : 2,
+    VIEW_MIN_SCALE : 0.8,
+
+    VIEW_LOGICAL_LEFT : -1,
+    VIEW_LOGICAL_RIGHT : 1,
+
+    VIEW_LOGICAL_MAX_LEFT : -2,
+    VIEW_LOGICAL_MAX_RIGHT : 2,
+    VIEW_LOGICAL_MAX_BOTTOM : -2,
+    VIEW_LOGICAL_MAX_TOP : 2,
+
+    PRIORITY_NONE : 0,
+    PRIORITY_IDLE : 1,
+    PRIORITY_NORMAL : 2,
+    PRIORITY_FORCE : 3,
+
+    MODEL_MURAKUMO : "resource/Live2D/murakumo/murakumo.model.json",
+
+    MOTION_GROUP_IDLE : "idle",
+    MOTION_GROUP_TAP : "tap",
+    MOTION_GROUP_TAP_EAR : "tap_ear",
+    MOTION_GROUP_TAP_BUST : "tap_bust",
+
+    HIT_AREA_HEAD : "head",
+    HIT_AREA_EAR_L  : "ear_l",
+    HIT_AREA_EAR_R  : "ear_r",
+    HIT_AREA_BUST : "bust"
+
+};
