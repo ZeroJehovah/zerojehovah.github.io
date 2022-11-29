d70f5aa3916badb9aa81584e4b40f8781dea2c58
IceRovah <icerovah@gmail.com>
2022-11-29 16:07:17 +0800

    20221129

---
+++ genshin/note/js/config.js
@@ -0,0 +1,46 @@
+const FOLLOW_ROLE = ["刻晴", "心海", "安柏", "宵宫", "纳西妲"];
+const FOLLOW_WEAPON = ["试作金珀", "弓藏", "千夜浮梦"];
+const ACHIEVEMENT = [{
+    title: "蒙德委托成就",
+    url: "https://bbs.mihoyo.com/ys/article/27622995",
+    achievements: [{
+        title: "西风佑我",
+        description: "在委托任务「惊喜大礼」中，选择以下选项",
+        actions: [{
+            title: "玛乔丽",
+            finish: true
+        }, {
+            title: "昆恩",
+            finish: true
+        }, {
+            title: "莎拉",
+            finish: false
+        }, {
+            title: "薇尔",
+            finish: false
+        }]
+    },{
+        title: "过量的思念",
+        description: "完成5次委托任务「愿风带走思念」",
+        actions: [{
+            title: "2/5",
+            finish: false
+        }]
+    },{
+        title: "略表歉意",
+        description: "在委托任务「鸽子、鸭子、小孩子」中，杀死鸭子"
+    },{
+        title: "有一说一",
+        description: "在委托任务「来自冬天的故事」中探听到所有关于至冬国的情报",
+        actions: [{
+            title: "愚人众",
+            finish: false
+        }, {
+            title: "邪眼",
+            finish: false
+        }, {
+            title: "女皇陛下",
+            finish: true
+        }]
+    }]
+}];
