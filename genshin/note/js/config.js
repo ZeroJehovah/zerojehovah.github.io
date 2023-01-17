9be43206fd5bad5187f79c5492e8445d2dc5b0e9
IceRovah <icerovah@gmail.com>
2023-01-17 16:36:50 +0800

    'merge'

--- genshin/note/js/config.js
+++ genshin/note/js/config.js
@@ -1,55 +1,8 @@
-<<<<<<< HEAD
 let CONFIG;
 let FOLLOW_ROLE;
 let FOLLOW_WEAPON;
 let ACHIEVEMENT;
 let token;
-=======
-const FOLLOW_ROLE = ["夜兰", "云堇"];
-const FOLLOW_WEAPON = ["试作金珀", "西风猎弓"];
-const ACHIEVEMENT = [{
-    title: "蒙德委托成就",
-    url: "https://bbs.mihoyo.com/ys/article/27622995",
-    achievements: [{
-        title: "西风佑我",
-        description: "在委托任务「惊喜大礼」中，选择以下选项",
-        actions: [{
-            title: "玛乔丽",
-            finish: true
-        }, {
-            title: "昆恩",
-            finish: true
-        }, {
-            title: "莎拉",
-            finish: true
-        }, {
-            title: "薇尔",
-            finish: false
-        }]
-    },{
-        title: "过量的思念",
-        description: "完成5次委托任务「愿风带走思念」",
-        actions: [{
-            title: "3/5",
-            finish: false
-        }]
-    },{
-        title: "有一说一",
-        description: "在委托任务「来自冬天的故事」中探听到所有关于至冬国的情报",
-        actions: [{
-            title: "愚人众",
-            finish: false
-        }, {
-            title: "邪眼",
-            finish: true
-        }, {
-            title: "女皇陛下",
-            finish: true
-        }]
-    }]
-}];
-const MUSIC_NOT_GET = [2, 5, 6, 9, 12, 17, 18, 20, 21, 22, 25, 29, 30, 35, 36, 37, 41, 42, 43, 44, 46, 55, 58, 60, 61, 62, 63, 64, 65, 66, 67, 68, 70, 71];
->>>>>>> d92133beb9304af8369c434113db7cf1b871e917
 
 $(function () {
     get_config();
